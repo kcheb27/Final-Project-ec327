ef8eb60e47951137e971a0f1ea525a5c72b82db6
noodlez102 <arthur.hua.102@gmail.com>
2022-12-10 17:20:12 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -20,6 +20,18 @@ floor_list5 = [Floor(9,10,1),Floor(0,10,2),Floor(23,40,3),Floor(13,30,4),Floor(1
 Yawkey = Building(floor_list5,6,"School","Food","Yawkey","100 Bay State Road")
 
 
+floor_list13 = [Floor(165,200,1),Floor(185,200,2)]
+Howard_Therman_Center = Building(floor_list13, 5, "Study", "N/a", "Howard Therman Center", "808 Commonwealth Ave")
+floor_list14 = [Floor(35,50,0),Floor(18,30,1), Floor(12,30,2),Floor(8,20,3),Floor(7,30,4),Floor(9,20,5)]
+CAS = Building(floor_list14, 5, "School", "N/a", "CAS", "725 Commonwealth Ave")
+floor_list15 = [Floor(45,80,1),Floor(22,40,2), Floor(18,30,3)]
+CFA = Building(floor_list15,5,"School","N/a","CFA", "855 Commonwealth Ave")
+floor_list16 = [Floor(22,40,1),Floor(18,30,2), Floor(11,20,3)]
+CGS = Building(floor_list16,4, "School","N/a","CGS","871 Commonwealth Ave")
+floor_list17 = [Floor(18,30,1),Floor(22,40,2)]
+COM = Building(floor_list17, 3, "School","N/a","COM","640 Commonwealth Ave")
+floor_list18 = [Floor(6,10,1),Floor(65,100,2)]
+EPIC = Building(floor_list18, 2, "School", "N/a", "EPIC", "750 Commonwealth Ave")
 
 
 app = Flask(__name__)
